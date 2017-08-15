33c6514a1f5ec661fee3e5066a798797a8c47f51
kenshinji <gundam0083ster@gmail.com>
2017-08-15 17:51:53 +0800

    Add chalk for colorful display

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const chalk = require('chalk');
+//maybe we can add chalk for colorful display.
 
-
+//word should be read from outside
 const word = 'very';
 const URL = `http://dict.youdao.com/w/eng/${word}`
-const options =
+
 
 request({
   'url':URL,
   'proxy':'http://web-proxy.jp.hpicorp.net:8080'
 },(error, response, body)=>{
-  //console.log(cheerio.load(body)('div.trans-container'));
   const $ = cheerio.load(body);
-  console.log($('div#phrsListTab > div.trans-container > ul').text());
+  console.log(chalk.green($('div#phrsListTab > div.trans-container > ul').text()));
 });
