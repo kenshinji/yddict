210dc699ed00414a458be6d1657cc236ee145e14
kenshinji <gundam0083ster@gmail.com>
2017-08-15 17:40:22 +0800

    Extract word definition from response

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const request = require('request');
+const cheerio = require('cheerio');
+
 
 const word = 'very';
 const URL = `http://dict.youdao.com/w/eng/${word}`
+const options =
 
-request(URL,(error, response, body)=>{
-  console.log('error:',error);
-  console.log('statusCode:', response.statusCode);
-  console.log('body:', body);
+request({
+  'url':URL,
+  'proxy':'http://web-proxy.jp.hpicorp.net:8080'
+},(error, response, body)=>{
+  //console.log(cheerio.load(body)('div.trans-container'));
+  const $ = cheerio.load(body);
+  console.log($('div#phrsListTab > div.trans-container > ul').text());
 });
