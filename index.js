527c0c564ebfe918e9721258410e896384cf7c2d
kenshinji <gundam0083ster@gmail.com>
2017-08-16 12:38:59 +0800

    Make searching word as parameter

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+#!/usr/bin/env node
 const request = require('request');
 const cheerio = require('cheerio');
 const chalk = require('chalk');
 //maybe we can add chalk for colorful display.
 
 //word should be read from outside
-const word = 'very';
+const word = process.argv.slice(2)[0];
+//const word = 'very';
 const URL = `http://dict.youdao.com/w/eng/${word}`
 
 
@@ -13,5 +15,5 @@ request({
   'proxy':'http://web-proxy.jp.hpicorp.net:8080'
 },(error, response, body)=>{
   const $ = cheerio.load(body);
-  console.log(chalk.green($('div#phrsListTab > div.trans-container > ul').text()));
+  console.log(chalk.blue($('div#phrsListTab > div.trans-container > ul').text()));
 });
