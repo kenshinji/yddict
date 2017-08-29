a816bf9a38e812d1221a1eb0be7430075ccafca0
kenshinji <gundam0083ster@gmail.com>
2017-08-29 08:02:32 +0800

    Add color config and processing spinner

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const cheerio = require('cheerio');
 const chalk = require('chalk');
 const fs = require('fs');
 
+var Spinner = require('cli-spinner').Spinner;
+
+var spinner = new Spinner('searching... %s');
+spinner.setSpinnerString('|/-\\');
+spinner.start();
+
+let color = 'yellow';
 const home = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 const configFile = home + "/config.json";
 
@@ -25,12 +32,19 @@ const options = {
   'url':URL
 };
 
-if(config && config.proxy){
-  options.proxy = config.proxy;
+if(config){
+  if(config.proxy){
+    options.proxy = config.proxy;
+  }
+  if(config.color){
+    color = config.color;
+  }
 }
 
-
+const color_output = chalk.keyword(color);
 request(options,(error, response, body)=>{
+
   const $ = cheerio.load(body);
-  console.log(chalk.blue($('div#phrsListTab > div.trans-container > ul').text()));
+  spinner.stop(true);
+  console.log(color_output($('div#phrsListTab > div.trans-container > ul').text()));
 });
