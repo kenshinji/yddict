aba5ada2aa2303afcbc855e720aafa02ae5b5a81
kenshinji <gundam0083ster@gmail.com>
2017-09-08 13:16:12 +0800

    Add Chinese -> English query support.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cheerio = require('cheerio');
 const chalk = require('chalk');
 const fs = require('fs');
 const Spinner = require('cli-spinner').Spinner;
+const isChinese = require('is-chinese')
+const urlencode = require('urlencode');
 const spinner = new Spinner('努力查询中... %s');
 const home = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 const configFile = home + "/config.json";
@@ -29,7 +31,9 @@ const readFile = (filename, encoding) => {
 const config = JSON.parse(readFile(configFile,"utf8"));
 
 const word = process.argv.slice(2)[0];
-const URL = `http://dict.youdao.com/w/${word}`
+const isCn = isChinese(word);
+const URL = isCn ? `http://dict.youdao.com/w/eng/${urlencode(word)}`:`http://dict.youdao.com/w/${word}`
+//const URL = 'http://dict.youdao.com/w/eng/%E5%A5%BD'
 const options = {
   'url':URL
 };
@@ -47,6 +51,16 @@ const color_output = chalk.keyword(color);
 request(options,(error, response, body)=>{
 
   const $ = cheerio.load(body);
+
+  //console.log(response);
   spinner.stop(true);
-  console.log(color_output($('div#phrsListTab > div.trans-container > ul').text()));
+  if(isCn){
+    $('div.trans-container > ul').find('p.wordGroup').each(function(i,elm){
+      var line = $(this).text().replace(/\s+/g," ");
+      console.log(color_output(line));
+    });
+  }else{
+    console.log(color_output($('div#phrsListTab > div.trans-container > ul').text()));
+  }
+
 });
