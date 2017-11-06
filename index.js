5b97dc0be9e3eb94c2424ae9c9b2b5ad79a7846e
李木 <limuxljy@dingding.com>
2017-11-06 20:40:30 +0800

    修改一部分eslint 规则

--- index.js
+++ index.js
@@ -18,15 +18,18 @@ const input = argv._;
 const word = input.join(' '); // 用户输入的搜索字符
 
 // 判断是否输入内容
-if (word.trim().length === 0) {
-  console.log('please input the word to translate\n请输入要翻译的内容')
-  return
+
+function main () {
+  // 如果输入是空
+  if (word.trim().length === 0) {
+    console.log('please input the word to translate\n请输入要翻译的内容')
+    return
+  }
+  // 如果输入了-s参数
+  if(argv.search) {
+    search.searchEng(word)
+    return
+  }
+  translate.translateWord(word)
 }
-
-
-if(argv.search) {
-  search.searchEng(word)
-  return
-}
-
-translate.translateWord(word)
+main()
