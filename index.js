8682db6220c175b10d58f3710db8832ca0a6ad66
kenshinji <gundam0083ster@gmail.com>
2018-03-07 22:24:19 +0800

    Use configstore for custom configuration.

--- index.js
+++ index.js
@@ -7,46 +7,35 @@ const fs = require('fs');
 const Spinner = require('cli-spinner').Spinner;
 const isChinese = require('is-chinese')
 const urlencode = require('urlencode');
+
+const pkg = require('./package.json');
+const Configstore = require('configstore');
+var conf = new Configstore(pkg.name, { color : 'white'});
+
 const spinner = new Spinner('努力查询中... %s');
-const home = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
-const configFile = home + "/config.json";
-let color = 'white';
 
 spinner.setSpinnerString('|/-\\');
 spinner.start();
 
-const readFile = (filename, encoding) => {
-
-    try {
-        return fs.readFileSync(filename).toString(encoding);
-    }
-    catch (e) {
-        return null;
-    }
-};
-
-const config = JSON.parse(readFile(configFile,"utf8"));
-
 const input = process.argv.slice(2)
 const word = input.join(' ')
-const isCn = isChinese(word);
-const URL = isCn ? `http://dict.youdao.com/w/eng/${urlencode(word)}`:`http://dict.youdao.com/w/${urlencode(word)}`
+
 
 const options = {
-  'url':URL
+  'url':isChinese(word) ? `http://dict.youdao.com/w/eng/${urlencode(word)}`:`http://dict.youdao.com/w/${urlencode(word)}`
 };
 
-if(config){
-  if(config.proxy){
-    options.proxy = config.proxy;
-  }
-  if(config.color){
-    color = config.color;
-  }
+if(conf.get('proxy')){
+    options.proxy = conf.get('proxy')
 }
+
+let color = conf.get('color');
+
 
 const color_output = chalk.keyword(color);
 request(options,(error, response, body)=>{
+
+  //parse response
   const $ = cheerio.load(body, {
     ignoreWhitespace: true,
     xmlMode: true
@@ -54,7 +43,7 @@ request(options,(error, response, body)=>{
   let result = '';
 
   spinner.stop(true);
-  if(isCn){
+  if(isChinese(word)){
     $('div.trans-container > ul').find('p.wordGroup').each(function(i,elm){
       result = $(this).text().replace(/\s+/g," ");
     });
