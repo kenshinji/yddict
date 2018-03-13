130af9b3fbcf873468c66e925bcb2ae7c129d127
kenshinji <gundam0083ster@gmail.com>
2018-03-13 10:41:36 +0800

    Add unit test for config.js

--- test/config.test.js
+++ test/config.test.js
@@ -1,7 +1,10 @@
-const should = require('chai').should()
+const expect =require('chai').expect
+const config = require('../lib/config')
 
 describe('Unit tests for config', () => {
 	it('Tests for reading configurations', (done) => {
+	  expect(config.proxy === undefined).to.be.true
+		expect(config.color).to.equal('white')
 		done()
 	})
 })
