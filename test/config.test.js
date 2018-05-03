383b24e924e437039a7ba285bb348e27b647d790
Jim <easonjim@163.com>
2018-05-03 11:21:12 +0800

    1、翻译开头增加音标显示 2、查询时的提示增加配置文件可配置，方便对接第三方系统，默认为提示

--- test/config.test.js
+++ test/config.test.js
@@ -3,7 +3,9 @@ const config = require('../lib/config')
 
 describe('Unit tests for config', () => {
 	it('Tests for reading configurations', (done) => {
+		console.log(config.spinner)
 	  expect(config.proxy === undefined).to.be.true
+		expect(config.spinner === true).to.be.true
 		expect(config.color).to.equal('white')
 		expect(config.getURL('hello')).to.equal('http://dict.youdao.com/w/')
 		expect(config.getURL('你好')).to.equal('http://dict.youdao.com/w/eng/')
--- test/parser.test.js
+++ test/parser.test.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect
 const Parser = require('../lib/parser')
 const fs = require('fs');
 const path = require('path')
-const expected_output = ' int. 喂；哈罗 n. 表示问候， 惊奇或唤起注意时的用语 n. (Hello)人名；(法)埃洛 '
+const expected_output = '英 [həˈləʊ] 美 [helˈō]  int. 喂；哈罗 n. 表示问候， 惊奇或唤起注意时的用语 n. (Hello)人名；(法)埃洛 '
 
 
 describe('Unit tests for parser', () => {
