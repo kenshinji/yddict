4fa271502dcef7fb1e6cb3735dd05083263e35ae
kenshinji <gundam0083ster@gmail.com>
2018-06-11 15:30:05 +0800

    Fix lint errors

--- test/config.test.js
+++ test/config.test.js
@@ -1,10 +1,13 @@
-const expect =require('chai').expect
+const dirtyChai = require('dirty-chai')
 const config = require('../lib/config')
+const chai = require('chai')
+const expect = chai.expect
+chai.use(dirtyChai)
 
 describe('Unit tests for config', () => {
 	it('Tests for reading configurations', (done) => {
-		expect(config.proxy === undefined).to.be.true
-		expect(config.spinner === true).to.be.true
+		expect(config.proxy === undefined).to.be.true()
+		expect(config.spinner === true).to.be.true()
 		expect(config.color).to.equal('white')
 		expect(config.getURL('hello')).to.equal('http://dict.youdao.com/w/')
 		expect(config.getURL('你好')).to.equal('http://dict.youdao.com/w/eng/')
--- test/parser.test.js
+++ test/parser.test.js
@@ -1,14 +1,13 @@
 const expect = require('chai').expect
 const Parser = require('../lib/parser')
-const fs = require('fs');
+const fs = require('fs')
 const path = require('path')
-const expected_output = '英 [həˈləʊ] 美 [helˈō]  int. 喂；哈罗 n. 表示问候， 惊奇或唤起注意时的用语 n. (Hello)人名；(法)埃洛 '
-
+const expectedOutput = '英 [həˈləʊ] 美 [helˈō]  int. 喂；哈罗 n. 表示问候， 惊奇或唤起注意时的用语 n. (Hello)人名；(法)埃洛 '
 
 describe('Unit tests for parser', () => {
 	it('Test for parsing html page content', (done) => {
 		const body = fs.readFileSync(path.join(__dirname, '../test/fixtures/body.html'))
-		expect(Parser.parse(false, body)).to.equal(expected_output)
-		done() 
+		expect(Parser.parse(false, body)).to.equal(expectedOutput)
+		done()
 	})
 })
