996dae7c41cb9610c06e3943dc0bede63506448e
changtui <yinsong.huo@duitang.com>
2018-09-03 15:05:17 +0800

    颜色，例句可配置

--- index.js
+++ index.js
@@ -10,8 +10,25 @@ const config = require('./lib/config')
 const Parser = require('./lib/parser')
 
 let word = process.argv.slice(2).join(' ')
+
+let argv = process.argv[2]
+
 if (!word) {
 	console.log('Usage: yd <WORD_TO_QUERY>')
+	process.exit()
+}
+
+if (argv === '-c') {
+	let color = process.argv[3]
+	config.setColor(color)
+	console.log(`设置成功，颜色为： ${color} `)
+	process.exit()
+}
+
+if (argv === '-e') {
+	let isShow = process.argv[3]
+	config.setEg(isShow)
+	console.log(`设置成功，例句显示为： ${isShow} `)
 	process.exit()
 }
 
@@ -40,5 +57,5 @@ request(options, (error, response, body) => {
 	if (config.spinner) {
 		spinner.stop(true)
 	}
-	console.log(ColorOutput(Parser.parse(isCN, body)))
+	console.log(ColorOutput(Parser.parse(isCN, body, config.eg)))
 })
