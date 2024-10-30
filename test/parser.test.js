2c16b4f586ab90ce1e92313e4f449b77af528678
Fred Wang <frwa@monta.com>
2024-10-30 23:36:47 +0100

    Refactor parser.js and update tests.

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,19 +1,38 @@
 const expect = require('chai').expect
-const Parser = require('../lib/parser')
+const { parser, format } = require('../lib/parser')
 const fs = require('fs')
 const path = require('path')
-const expectedOutput = `英 [həˈləʊ] 美 [helˈō] \n\nint. 喂；哈罗\nn. 表示问候， 惊奇或唤起注意时的用语\nn. (Hello)人名；(法)埃洛\n\nHello, who\'s speaking, please?\n喂， 请问你是谁呀?\nThe American walked to a telephone booth,"Hello. Is that the bank?\n那个美国人走到公用电话间旁打电话:"喂,银行吗?\nShe never passes without stopping to say hello.\n她从这儿经过时没有一次不停下来问候一番。\n`
-const expectedOutput2 = `\n\n hello ; hi ; how do you do \n`
+
 describe('Unit tests for parser', () => {
-	it('Test for parsing html page content when word is not Chinese', (done) => {
+	it('Should parse English word correctly', (done) => {
 		const body = fs.readFileSync(path.join(__dirname, '../test/fixtures/body.html'))
-		expect(Parser.parse(false, body)).to.equal(expectedOutput)
+		const result = parser(body, 'hello')
+
+		expect(result).to.be.an('object')
+		if (result.pronounces && result.pronounces.length) {
+			expect(result.pronounces).to.be.an('array')
+			const output = format(result)
+			expect(output).to.include('发音')
+		}
+
+		expect(result).to.have.property('translations')
+		expect(result.translations).to.be.an('array')
+		const output = format(result)
+		expect(output).to.be.a('string')
+		expect(output).to.include('翻译')
 		done()
 	})
 
-	it('Test for parsing html page content when word is Chinese', (done) => {
+	it('Should parse Chinese word correctly', (done) => {
 		const body = fs.readFileSync(path.join(__dirname, '../test/fixtures/cn-body.html'))
-		expect(Parser.parse(true, body)).to.equal(expectedOutput2)
+		const result = parser(body, '你好')
+
+		expect(result).to.have.property('translations')
+		expect(result.translations).to.be.an('array')
+
+		const output = format(result)
+		expect(output).to.be.a('string')
+		expect(output).to.include('翻译')
 		done()
 	})
 })
